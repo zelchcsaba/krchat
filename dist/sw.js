52a9320866d3e2aa6dba0bfbf5bfaeddc90be3b6
Zelch Csaba <zch03po@gmail.com>
2025-11-10 15:26:50 +0100

    lebuildelve

--- dist/sw.js
+++ dist/sw.js
@@ -1,13 +1,26 @@
 const cacheName = "v1";
+
+// --- Cache-implementáció ---
 async function impl(e) {
-    let cache = await caches.open(cacheName); // Cache megnyitása, async
-    let cacheResponse = await cache.match(e.request); // Lookup
-    if (cacheResponse) // Ha megvan
-        return cacheResponse // Visszadjuk
-    else {
-        let networkResponse = await fetch(e.request); // Ha nincs meg, akkor elindítjuk a tényleges hálózati lekérdezést
-        cache.put(e.request, networkResponse.clone()) // Eltároljuk
-        return networkResponse; // Visszadjuk
+    let cache = await caches.open(cacheName);
+    let cacheResponse = await cache.match(e.request);
+    if (cacheResponse) {
+        return cacheResponse;
+    } else {
+        let networkResponse = await fetch(e.request);
+        cache.put(e.request, networkResponse.clone());
+        return networkResponse;
     }
 }
-self.addEventListener("fetch", e => e.respondWith(impl(e))); // Eseményre feliratkozás
+self.addEventListener("fetch", e => e.respondWith(impl(e)));
+
+self.addEventListener("push", e => {
+
+    const dataText = e.data?.text() || "Új üzenet érkezett!";
+
+    const promiseChain = self.registration.showNotification("Chat Notification", {
+        body: dataText,
+    });
+
+    e.waitUntil(promiseChain);
+});
